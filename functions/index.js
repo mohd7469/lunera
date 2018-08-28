38bbd5911fb380a1998cf250cb650d0c0e935bfa
awais786327 <awais3227443@gmail.com>
2018-08-28 19:31:02 +0500

    init index

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,36 @@
+
+'use strict';
+
+/* Initialize firebase admin */
+const admin = require('firebase-admin');
+
+/* Import the firebase-functions package for deployment */
 const functions = require('firebase-functions');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+/* Initialize firebase functions */
+admin.initializeApp(functions.config().firebase);
+
+/* Initialize firebase database */
+var db = admin.firestore();
+
+/* Import Dialogflow module from the Actions on Google client library */
+const {
+  SimpleResponse,
+  dialogflow,
+  } = require('actions-on-google');
+
+/* Instantiate Dialogflow client */
+const app = dialogflow();
+
+app.intent('Default Welcome Intent', (conv) => {
+
+  conv.ask(new SimpleResponse({
+    speech: `Welcome to the Lunera`,
+    text: `Welcome to the Lunera`
+  }));
+
+  conv.add('hello');
+
+});
+
+exports.dialogflowFirebaseFulfillment = functions.https.onRequest(app);
